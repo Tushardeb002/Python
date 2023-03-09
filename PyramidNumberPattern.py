f257e66b29bf1c55ab14436944072e0ac1d35db7
Tushar Deb <106898218+TDeb003@users.noreply.github.com>
2023-03-09 13:29:40 +0530

    Create PyramidNumberPattern.py
    Signed-off-by: Tushar Deb  <106898218+TDeb003@users.noreply.github.com>

---
+++ PyramidNumberPattern.py
@@ -0,0 +1,33 @@
+from os import*
+from sys import*
+from collections import*
+from math import*
+
+from os import*
+from sys import*
+from collections import*
+from math import*
+
+from os import*
+from sys import*
+from collections import*
+from math import*
+
+from os import*
+from sys import*
+from collections import*
+from math import*
+
+from os import*
+from sys import*
+from collections import*
+from math import*
+
+n=int(input())
+for i in range(1,n+1):
+  print("  "*(n-i), end=" ")
+  for j in range(i,0,-1):
+    print(j,end=" ")
+  for k in range(2,i+1):
+    print(k,end=" ")
+  print()  
